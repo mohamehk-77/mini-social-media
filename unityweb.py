d80b71f74f382ff5270668864a7bfa5b5355afd6
mohamed kamal <kmido9729@gmail.com>
2024-04-21 20:26:32 +0200

    so

---
+++ unityweb.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://username:password@localhost:3306/mini_social_media'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Define models here
